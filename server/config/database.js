598d6d5480816277ae6e9c3d4b0dcc5674aac913
Francois <franksauve@outlook.com>
2017-09-25 22:23:48 -0400

    Heroku with mysql

--- server/config/database.js
+++ server/config/database.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 
 var dbConfig = {
-    host: 'us-cdbr-iron-east-05',
+    host: 'us-cdbr-iron-east-05.cleardb.net',
     user: 'b2486b8bd3fd87',
     password: 'c2424e78',
     database: 'heroku_f09d70d39831307'
