62bebd000e4eb2228ceb9fe3d682189a5adf3ea2
hassanahmed95 <hassan.514@hotmail.com>
2017-09-27 16:10:13 -0400

    Added request validation and database query for adding a product to the database

--- server/routes/products.js
+++ server/routes/products.js
@@ -21,7 +21,25 @@ db.getConnection((err, connection) => {
       if (err){
         return res.json(401, {success: false, msg: "Unauthorized: Incorrect Token Signature"});
       } else {
-        return res.json(201, {success: true, msg: "New Product Created"});
+        let newProduct = req.body;
+        let category = newProduct.category;
+
+        //verify if the category is valid
+        if (!category.match(/^(DesktopComputer|TabletComputer|Laptop|Television)$/)){
+          return res.json(400, {success: false, msg: "Invalid product category."});
+        }
+
+        //Build query and add new product in the db
+        let sql = `INSERT INTO ' +${category}' SET ?`;
+        connection.query(sql, newProduct, (err, result) => {
+            if(err){
+                console.log(err);
+                return res.json(500, {success: false, msg: "New Product Could Not Be Added"});
+            }
+            else{
+                return res.json(201, {success: true, msg: "New Product Has Been Added"});
+            }
+        });
       }
     });
   });
