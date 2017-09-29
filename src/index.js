1d1f09c0bd70a756a5b7c129c3a5810dd400c5e3
Francois <franksauve@outlook.com>
2017-09-29 13:13:19 -0400

    Basic unit tests

--- src/index.js
+++ src/index.js
@@ -6,6 +6,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './js/App';
-
+require ('./scss/App.scss');
 
 ReactDOM.render(<App />, document.getElementById('root'));
