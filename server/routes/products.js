4cd4f623bc38c3cb7461fb58864c410fe15eee09
hassanahmed95 <hassan.514@hotmail.com>
2017-09-29 13:13:12 -0400

    Add products route has been modified to accept the new format of the request body as provided by the front end, skeleton for get all products route has been implemented

--- server/routes/products.js
+++ server/routes/products.js
@@ -6,8 +6,25 @@ const express = require('express'),
     db = require('../config/database.js');
 
 db.getConnection((err, connection) => {
+  // Get all products
+  products.get('/view', (req, res) => {
+    //Authenticate the request using jwt
+    //Verify if a token is provided
+    var token = req.headers.authorization;
+    if (!token){
+      return res.json(401, {success: false, msg: "Unauthorized: No Token Provided"});
+    }
 
-  // Register user in the db
+    //Validate token signature
+    jwt.verify(token, 'mysecret', function(err, decoded) {
+      if (err){
+        return res.json(401, {success: false, msg: "Unauthorized: Incorrect Token Signature"});
+      } else {
+        
+      }
+  });
+
+  // Add a product in the db
   products.post('/add', (req, res) => {
     //Authenticate the request using jwt
     //Verify if a token is provided
@@ -21,16 +38,20 @@ db.getConnection((err, connection) => {
       if (err){
         return res.json(401, {success: false, msg: "Unauthorized: Incorrect Token Signature"});
       } else {
-        let newProduct = req.body.product;
+        //verify if the category is valid
         let category = req.body.category;
+        if (!category.match(/^(desktopComputer|tablet|laptop|television)$/)){
+          return res.json(400, {success: false, msg: "Invalid product category."});
+        }
+
+        // build newProduct JSON Object by parsing the request
+        let newProduct = req.body.description;
+        newProduct.Price = req.body.price;
+        newProduct.Available = req.body.available;
+
         let newCatalogueItem = {
           SerialNumber : newProduct.SerialNumber,
           Available : newProduct.Available
-        }
-
-        //verify if the category is valid
-        if (!category.match(/^(desktopComputer|tablet|laptop|television)$/)){
-          return res.json(400, {success: false, msg: "Invalid product category."});
         }
 
         //Build query and add new catalogue entry
@@ -41,6 +62,7 @@ db.getConnection((err, connection) => {
                 return res.json(500, {success: false, msg: "New Catalogue Item Could Not Be Added"});
             }
             else{
+              console.log("New item has been added to catalogue table");
               //Build query and add new product in the db
               sql = `INSERT INTO ${category} SET ?`;
               connection.query(sql, newProduct, (err, result) => {
