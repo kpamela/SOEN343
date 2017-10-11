ef0c2c6684cc497531b1deb62504bd1eb917ed67
Raph1105 <Raph1105@hotmail.com>
2017-10-10 23:11:22 -0400

    Update Products.js

--- server/models/Products.js
+++ server/models/Products.js
@@ -3,7 +3,7 @@
 // Table structure for table 'Products' 
 let sql = `CREATE TABLE IF NOT EXISTS Products(
 	SerialNumber int(9) NOT NULL, 
-	ModelNumber varchar(9) NOT NULL, 
+	ModelNumber varchar(10) NOT NULL, 
     Available boolean, 
     PRIMARY KEY (SerialNumber, ModelNumber)
 )`
