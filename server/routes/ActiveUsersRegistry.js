d8be1d6d9973b0e3818e2309b5b476148d6a90bc
Raph1105 <Raph1105@hotmail.com>
2017-10-11 12:40:18 -0400

    Update and rename UserActivity.js to ActiveUsersRegistry.js

--- server/routes/ActiveUsersRegistry.js
+++ server/routes/ActiveUsersRegistry.js
@@ -1,14 +1,14 @@
 const express = require('express'),
     users = express.Router(),
     mysql = require('mysql'),
-    table = require('../models/UserActivity.js'),
+    table = require('../models/ActiveUsersRegistry.js'),
     bcrypt = require('bcryptjs'),
     jwt = require('jsonwebtoken'),
     db = require('../config/database.js');
 
 db.getConnection((err, connection) => {
     
-    // Create the UserActivity table
+    // Create the ActiveUsersRegistry table
     users.get('/createtable', (req, res) => {
         let sql = table;
         connection.query(sql, (err, result) => {
