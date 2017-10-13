e72f6bbfdb95e63fee9f4e7cf2f9c3b6170a6f44
CharlesPhilippeLabbe <c.p.labbe@me.com>
2017-10-13 15:53:11 -0400

    Refactoring for merging changes

--- src/js/NavBar/LoginModal.js
+++ src/js/NavBar/LoginModal.js
@@ -3,7 +3,7 @@ import {Modal, Button, FieldGroup} from 'react-bootstrap';
 import axios from 'axios';
 import auth from '../General/auth.js'
 import {Redirect, Route, BrowserRouter, Switch} from 'react-router-dom';
-import Catalog from '../Dashboard/Catalog.js'
+import Catalog from '../Dashboard/Dashboard.js'
 
 
 export const LoginModal = React.createClass({
