977f3d07b6a614ecdfefd4342c8f27c3970e67ff
CharlesPhilippeLabbe <c.p.labbe@me.com>
2017-10-20 12:45:53 -0400

    for refactor

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ var HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
 });
 
 module.exports = {
-    entry: __dirname + '/src/index.js',
+    entry: __dirname + '/presentation/index.js',
     module:{
         loaders: [
             {
