e194b2156f39cc27bb1f9f2bb3741f22bffe3715
Pamela Khouri <khouripamela@gmail.com>
2017-11-07 16:44:28 -0500

    Improved NavBar
    REmoved unecessary category drop down.
    Added Account page

--- src/js/NavBar/NavBar.js
+++ src/js/NavBar/NavBar.js
@@ -20,26 +20,22 @@ export default class NavBar extends Component{
           <Navbar.Toggle />
         </Navbar.Header>
         <Navbar.Collapse>
+
           <Nav>
             <NavItem eventKey={1}><Link className="NavbarLink" to="/">Home</Link></NavItem>
             <NavItem eventKey={2}><Link className="NavbarLink" to="/aboutus">About us</Link></NavItem>
-            <NavDropdown className="NavbarItem" eventKey={3} title="Shop" id="basic-nav-dropdown">
-              <NavDropdown eventKey={4} title="Computer Systems" id="basic-nav-dropdown">
-                <MenuItem eventKey={4.1}> Tablets </MenuItem>
-                <MenuItem eventKey={4.2}> Laptops </MenuItem>
-                <MenuItem eventKey={4.3}> Desktops </MenuItem>
-              </NavDropdown>
-              <MenuItem divider />
-              <MenuItem eventKey={3.3}><Link className="NavbarDropdownLink" to="/ClientDashboard">Shop all categories</Link></MenuItem>
-            </NavDropdown>
+            <NavItem eventKey={3}><Link className="NavbarLink" to="/ClientDashboard"> Shop </Link></NavItem>
             {auth.getIsAdmin() == 1 ? <NavItem><Link className="NavbarLink" to="/AdminDashboard">Administrator Dashboard</Link></NavItem> : null}
           </Nav>
           <Nav pullRight>
             {!auth.loggedIn() ? <NavItem eventKey={1} href="#"> <LoginModal/> </NavItem> : null}
             {!auth.loggedIn() ? <NavItem eventKey={2} href="#"> <RegisterModal/> </NavItem>: null}
-            {auth.loggedIn() ? <NavItem eventKey={3} href="#"> <MyAccount/> </NavItem>: null}
-            {auth.loggedIn() ? <NavItem eventKey={4} href="#"> <Logout /> </NavItem> : null}
-            {auth.loggedIn() ? <NavItem eventKey={5} href="#"><ShoppingCartModal/></NavItem> : null}
+            {auth.loggedIn() ? <NavDropdown className="NavbarItem" eventKey={3} title="My Account" id="basic-nav-dropdown">
+                                    <MenuItem eventKey={3.1}><Link className="NavbarDropdownLink" to="/AccountSettings"> Settings </Link></MenuItem>
+                                    <MenuItem eventKey={3.2}><Link className="NavbarDropdownLink" to="/PurchaseHistory"> Purchase history </Link></MenuItem>
+                                    <MenuItem eventKey={3.3}> <Logout /> </MenuItem>
+                               </NavDropdown>: null}
+            {auth.loggedIn() ? <NavItem eventKey={4} href="#"> <ShoppingCartModal/> </NavItem> : null}
 
           </Nav>
         </Navbar.Collapse>
