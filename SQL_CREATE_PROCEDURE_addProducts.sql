be773e0e6ad61b82f1ba80687a4ed7b1f3210c6e
Raph1105 <Raph1105@hotmail.com>
2017-11-07 13:31:30 -0500

    Update SQL_CREATE_PROCEDURE_addProducts.sql

--- SQL_CREATE_PROCEDURE_addProducts.sql
+++ SQL_CREATE_PROCEDURE_addProducts.sql
@@ -1,3 +1,4 @@
+-- This procedure will allow us to add a certain quantity "y" of products which will be refered to "x" modelNumber, and will generate their productId -- 
 CREATE PROCEDURE `addProducts`(x VARCHAR(20), y int(3))
 BEGIN 
 	DECLARE nb INT(9); 
